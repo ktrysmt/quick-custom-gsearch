6bf81e5b723998af6829e9918e78f0affbb7e8c2
ktrysmt <kotaro.yoshimatsu@gmail.com>
2016-12-14 17:11:25 +0900

    Fix some bugs and redesign the structure.
    Details are;
    - fix structure
    - fix manifest and script.js
    
    - redesign functions.
    
    - redefine namespaces
    
    - And fix some bugs.

--- src/script.js
+++ src/script.js
@@ -1,159 +1,196 @@
 (function(){
 
-    // define
-    var START = "";
-    var _to = new Date();
-    var END = _to.getFullYear() + "/" + (_to.getMonth() + 1) + "/" + _to.getDate();  
-    _to = undefined;  
+    var View = {
+      SetCssState: function (t) {
+        localStorage.setItem("quick-custom-gsearch",t);
+        var a = document.querySelectorAll(".quick-custom-gsearch a");
+        Array.prototype.map.call(a, function(e){
+            var i = e.getAttribute("data");
+            if (i === t) {
+              e.className = "active";
+            }
+            else {
+              e.className = "";
+            }
+        });
+      },
+      BindElement : function () {
+        // define
+        var term = {
+          0 : { data:"none", text:"Reset" },
+          1 : { data:"6month", text:"6 Month" },
+          2 : { data:"1year", text:"1 Year" },
+          3 : { data:"2year", text:"2 Year" },
+          4 : { data:"3year", text:"3 Year" }
+        };
+        var div = document.createElement("div");
+        div.className = "quick-custom-gsearch";
+        var a = [];
+        a[0] = document.createElement("a"),
+        a[1] = a[0].cloneNode(),
+        a[2] = a[0].cloneNode(),
+        a[3] = a[0].cloneNode(),
+        a[4] = a[0].cloneNode();
+        var span = [];
+        span[0] = document.createElement("span"),
+        span[1] = span[0].cloneNode();
+        // append
+        div.appendChild(span[0]);
+        Array.prototype.map.call(a, function(obj, i){
+            a[i].setAttribute("data", term[i]['data']);
+            a[i].appendChild(document.createTextNode(term[i]['text']));
+            a[i].addEventListener("click", View.QuickChange, false);
+            div.appendChild(a[i]); 
+        });
+        div.appendChild(span[1]);
+        // insert
+        var ucs = document.querySelector("div#ucs");
+        ucs.insertBefore(div, ucs.firstChild);
+      },    
+      QuickChange : function () {
+        var t = this.getAttribute("data");
+        Model.SetRangeToStorage(t);
+        var o = Browser.ParseURI();
+        var q = o["q"];
+        Browser.RewriteURI(t, q);
+      }
+    };
 
-    // funcs
-    function prependChild(parent, newChild) {
-      parent.insertBefore(newChild, parent.firstChild);
-    }
-    function setState(t) {
-      localStorage.setItem("quick-custom-gsearch",t);
-      var a = document.querySelectorAll(".quick-custom-gsearch a");
-      Array.prototype.map.call(a, function(e){
-          var i = e.getAttribute("data");
-          if (i === t) {
-            e.className = "active";
-          }
-          else {
-            e.className = "";
-          }
-      });
-    }
-    function setRange(t) {
-      var date = new Date();
-      if (t === "6month") {
-        date.setMonth( date.getMonth() - 6 ); 
-      }
-      else if (t === "1year") {
-        date.setFullYear( date.getFullYear() - 1 ); 
-      }
-      else if (t === "2year") {
-        date.setFullYear( date.getFullYear() - 2 ); 
+    var Model = {
+      START : "",
+      END : "",
+      SetRangeToStorage: function (range) {
+        localStorage.setItem("quick-custom-gsearch", range); 
+      },                        
+      GetRangeFromStorage: function () {
+        var t = localStorage.getItem("quick-custom-gsearch");
+        if (!t || ( t !== "none" && t !== "6month" && t !== "1year" && t !== "2year" && t !== "3year" )) { 
+          t = "none";
+          localStorage.setItem("quick-custom-gsearch", t); 
+        }
+        return t;
+      },
+      GetStartRange : function (t) {
+        var date = new Date();
+        if (t === "6month") {
+          date.setMonth( date.getMonth() - 6 ); 
+        }
+        else if (t === "1year") {
+          date.setFullYear( date.getFullYear() - 1 ); 
+        }
+        else if (t === "2year") {
+          date.setFullYear( date.getFullYear() - 2 ); 
+        } 
+        else if (t === "3year") {
+          date.setFullYear( date.getFullYear() - 3 ); 
+        } 
+        else {
+          return false;
+        }
+        var y = date.getFullYear();
+        var m = date.getMonth() + 1;
+        var d = date.getDate();
+        m = (m.toString().length === 1) ? "0" + m.toString() : m.toString();
+        d = (d.toString().length === 1) ? "0" + d.toString() : d.toString();
+        return y + "/" + m + "/" + d;
+      },
+      GetEndRange() {
+        var to = new Date();
+        return to.getFullYear() + "/" + (to.getMonth() + 1) + "/" + to.getDate();  
       } 
-      else if (t === "3year") {
-        date.setFullYear( date.getFullYear() - 3 ); 
-      } 
-      else {
-        return false;
+    };
+
+    var Browser = {
+      IsUCS : function () {
+        var ucs = document.querySelector("div#ucs");
+        if (ucs === null) {
+          return false;
+        }
+        return true;
+      },
+      IsAllSearch : function (o) {
+        if (typeof(o["tbm"]) === "undefined" || o["tbm"] === "") {
+          return true;
+        }
+        return false; 
+      }, 
+      ParseURI : function() {
+        var s = {};
+        var o = {};
+        if ( /^\/search$/.test(location.pathname) ) {
+          s = location.search.slice(1).split("&");
+        }
+        else if (location.pathname === "/" || /^\/webhp$/.test(location.pathname)) {
+          if (/#q=/.test(location.hash)) {
+            s = location.hash.split("#")
+          }
+          else if (/&q=/.test(location.hash)) {
+            s = location.hash.split("&")
+          }
+        }
+        for (var i = 0; i < s.length; ++i) {
+          var a = s[i].split('=');    
+          o[a[0]] = a[1];         
+        }
+        return o;
+      },
+      RewriteURI : function(t, q) {
+        var hash    = window.location.search;
+        Model.START = Model.GetStartRange(t);
+        Model.END   = Model.GetEndRange();
+        if (t === "none") {
+          var path = "/search?hl=ja&site=webhp&biw=810&bih=1306&q=" + q + "&oq=" + q + "&ie=UTF-8&tbm="; 
+          window.location.href = path;
+        }
+        else {
+          var range = "tbs=cdr:1,cd_min:" + Model.START + ",cd_max:" + Model.END
+          var path = "/search?hl=ja&site=webhp&biw=810&bih=1306&q=" + q + "&oq=" + q + "&ie=UTF-8&" + range + "&tbm="
+          window.location.href = path;
+        }
+      },
+      ReplaceToNeutralIfExistRange : function (){
+        var pathname = window.location.pathname;
+        var hash     = window.location.search;
+        var o        = Browser.ParseURI();
+        var q        = (typeof o["q"] !== 'undefined') ? o["q"] : "";
+        var pattern  = /(tbs=cdr:1,cd_min:)(.*?)(,cd_max:)/;
+        if( hash.match(pattern) && Browser.IsAllSearch(o) ) {
+          var path = "/search?hl=ja&site=webhp&biw=810&bih=1306&q=" + q + "&oq=" + q + "&ie=UTF-8&tbm=";
+          window.location.href = path;
+        }
       }
-      var m = date.getMonth() + 1;
-      var d = date.getDate();
-      m = (m.toString().length === 1) ? "0" + m.toString() : m.toString();
-      d = (d.toString().length === 1) ? "0" + d.toString() : d.toString();
-      START = date.getFullYear() + "/" + m + "/" + d;
-    }
-    function isPlainSearch() {
-      var vars = getHash();
-      if (typeof(vars["tbm"]) === "undefined" || vars["tbm"] === "") {
+    };
+
+    var Main = function () {
+
+      var CommonInit = function () {
+        var o = Browser.ParseURI();    
+        if (Browser.IsAllSearch(o) === true) {
+          var range = Model.GetRangeFromStorage();
+          View.BindElement();
+          View.SetCssState(range);
+        }
+        Model.SetRangeToStorage("none");
+      }
+
+      if (Model.GetRangeFromStorage() === "none") {
+        Browser.ReplaceToNeutralIfExistRange();
+      }
+
+      if (Browser.IsUCS() === false) {
+        var Observer = new MutationObserver( function(mutations) {
+            if (Browser.IsUCS() === true) CommonInit();
+        });
+        Observer.observe(document.getElementById("main"), { childList: true }); 
         return true;
       }
       else {
-        return false;
-      }
-    }
-    function getHash() {
-      var vars = [];
-      var hash = location.search.slice(1).split('&');    
-      for (var i = 0; i < hash.length; i++) {
-        var array = hash[i].split('=');    
-        vars.push(array[0]);               
-        vars[array[0]] = array[1];         
-      }  
-      return vars;
-    }
-
-    // trigger
-    function clickQuick() {
-      var t = this.getAttribute("data");
-      setState(t);
-      setRange(t);   
-      run(t);
-    } 
-    function run(t) {
-      var hash = window.location.search;
-      if (!t || ( t !== "none" && t !== "6month" && t !== "1year" && t !== "2year" && t !== "3year" )) {
-        return false;
-      }
-      var vars = getHash();
-      if (t === "none") {
-        var pattern = /(tbs=cdr:1,cd_min:)(.*?)(,cd_max:)/;
-        if( hash.match(pattern) ) {
-          window.location.href = "/search?hl=ja&site=webhp&biw=810&bih=1306&q=" + vars["q"] + "&oq=" + vars["q"] + "&ie=UTF-8&tbm="
-        }
-      }
-      else {
-        var range = "tbs=cdr:1,cd_min:" + START + ",cd_max:" + END
-        var q = "/search?hl=ja&site=webhp&biw=810&bih=1306&q=" + vars["q"] + "&oq=" + vars["q"] + "&ie=UTF-8&" + range + "&tbm="
-        if ( hash.indexOf(range) === -1 ) {
-          window.location.href = q;
-        } 
+        CommonInit();
+        return true;
       }
     }
 
-    // bind
-    function bindElement() {
-      var e = document.createElement("div");
-      var p = document.querySelector("div.mw");
-      var o0 = document.createElement("a");
-      var o1 = document.createElement("a");
-      var o2 = document.createElement("a");
-      var o3 = document.createElement("a");
-      var o4 = document.createElement("a");
-      var span1 = document.createElement("span");
-      var span2 = document.createElement("span");
-      e.className = "quick-custom-gsearch"
-
-      o0.setAttribute("href", "javascript:void(0);");
-      o0.setAttribute("data", "none");
-      o0.appendChild( document.createTextNode("Reset") );
-      o0.addEventListener("click",clickQuick,false);
-
-      o1.setAttribute("href", "javascript:void(0);");
-      o1.setAttribute("data", "6month");
-      o1.appendChild( document.createTextNode("6 Month") );
-      o1.addEventListener("click",clickQuick,false);
-
-      o2.setAttribute("href", "javascript:void(0);");
-      o2.setAttribute("data", "1year");
-      o2.appendChild( document.createTextNode("1 Year") ); 
-      o2.addEventListener("click",clickQuick,false);
-
-      o3.setAttribute("href", "javascript:void(0);");
-      o3.setAttribute("data", "2year");
-      o3.appendChild( document.createTextNode("2 Year") );
-      o3.addEventListener("click",clickQuick,false);
-
-      o4.setAttribute("href", "javascript:void(0);");
-      o4.setAttribute("data", "3year");
-      o4.appendChild( document.createTextNode("3 Year") );
-      o4.addEventListener("click",clickQuick,false);
-      e.appendChild(span1)
-      e.appendChild(o0);
-      e.appendChild(o1);
-      e.appendChild(o2);
-      e.appendChild(o3);
-      e.appendChild(o4);    
-      e.appendChild(span2)
-      prependChild(p,e)
-    }
-
-    // run
-    if (isPlainSearch()) {
-      bindElement();
-      var t = localStorage.getItem("quick-custom-gsearch");
-      if (!t || ( t !== "none" && t !== "6month" && t !== "1year" && t !== "2year" && t !== "3year" )) { 
-        t = "none";
-        localStorage.setItem("quick-custom-gsearch",t); 
-      }
-      setState(t);    
-      setRange(t);   
-      run(t);
-      localStorage.setItem("quick-custom-gsearch","none"); 
-    }
+    Main();
 
 })();
 
