0b4727bcdbd2243214266c0a06c53e96bd2a03a7
ktrysmt <kotaro.yoshimatsu@gmail.com>
2017-01-06 19:13:47 +0900

    Use es6 and remove functions to call LocalStorage.

--- src/script.js
+++ src/script.js
@@ -1,204 +1,116 @@
 (function () {
-	var View = {
-		SetCssState: function (t) {
-			localStorage.setItem('quick-custom-gsearch', t);
-			var a = document.querySelectorAll('.quick-custom-gsearch a');
-			Array.prototype.map.call(a, function (e) {
-				var i = e.getAttribute('data');
-				if (i === t) {
-					e.className = 'active';
-				}				else {
-					e.className = '';
-				}
-			});
-		},
-		BindElement: function () {
-        // define
-			var term = {
-				0: {data: 'none', text: 'Reset'},
-				1: {data: '6month', text: '6 Month'},
-				2: {data: '1year', text: '1 Year'},
-				3: {data: '2year', text: '2 Year'},
-				4: {data: '3year', text: '3 Year'}
-			};
-			var div = document.createElement('div');
-			div.className = 'quick-custom-gsearch';
-			var a = [];
-			a[0] = document.createElement('a'),
-        a[1] = a[0].cloneNode(),
-        a[2] = a[0].cloneNode(),
-        a[3] = a[0].cloneNode(),
-        a[4] = a[0].cloneNode();
-			var span = [];
-			span[0] = document.createElement('span'),
-        span[1] = span[0].cloneNode();
-        // append
-			div.appendChild(span[0]);
-			Array.prototype.map.call(a, function (obj, i) {
-				a[i].setAttribute('data', term[i].data);
-				a[i].appendChild(document.createTextNode(term[i].text));
-				a[i].addEventListener('click', View.QuickChange, false);
-				div.appendChild(a[i]);
-			});
-			div.appendChild(span[1]);
-        // insert
-			var ucs = document.querySelector('div#ucs');
-			ucs.insertBefore(div, ucs.firstChild);
-		},
-		QuickChange: function () {
-			var t = this.getAttribute('data');
-			Model.SetRangeToStorage(t);
-			var o = Browser.QueryHashToArray();
-			var q = o.q;
-			Browser.RewriteURI(t, q);
-		},
-		DeleteElement: function () { // TODO
-        // var e = document.querySelector(".quick-custom-gsearch");
-		}
-	};
+    const View = {
+      SetCssState: (range) => {
+        let a = document.querySelector('.quick-custom-gsearch a[data=' + range + ']');
+        a.className = 'active';
+      },
+      BindElement: () => {
+        const term    = Model.TERM;
+        const div       = document.createElement('div');
+        div.className = 'quick-custom-gsearch';
+        const span    = document.createElement('span');
 
-	var Model = {
-		START: '',
-		END: '',
-		SetRangeToStorage: function (range) {
-			localStorage.setItem('quick-custom-gsearch', range);
-		},
-		GetRangeFromStorage: function () {
-			var t = localStorage.getItem('quick-custom-gsearch');
-			if (!t || (t !== 'none' && t !== '6month' && t !== '1year' && t !== '2year' && t !== '3year')) {
-				t = 'none';
-				localStorage.setItem('quick-custom-gsearch', t);
-			}
-			return t;
-		},
-		GetStartRange: function (t) {
-			var date = new Date();
-			if (t === '6month') {
-				date.setMonth(date.getMonth() - 6);
-			}			else if (t === '1year') {
-				date.setFullYear(date.getFullYear() - 1);
-			}			else if (t === '2year') {
-				date.setFullYear(date.getFullYear() - 2);
-			}			else if (t === '3year') {
-				date.setFullYear(date.getFullYear() - 3);
-			}			else {
-				return false;
-			}
-			var y = date.getFullYear();
-			var m = date.getMonth() + 1;
-			var d = date.getDate();
-			m = (m.toString().length === 1) ? '0' + m.toString() : m.toString();
-			d = (d.toString().length === 1) ? '0' + d.toString() : d.toString();
-			return y + '/' + m + '/' + d;
-		},
-		GetEndRange() {
-			var to = new Date();
-			return to.getFullYear() + '/' + (to.getMonth() + 1) + '/' + to.getDate();
-		}
-	};
+        div.appendChild(span);
+        for (let i of Array(6).keys()) {
+          const elm = document.createElement('a')
+          elm.setAttribute('data', term[i].data);
+          elm.appendChild(document.createTextNode(term[i].text));
+          elm.addEventListener('click', View.QuickChange, false);
+          div.appendChild(elm);
+        }
+        div.appendChild(span.cloneNode());
 
-	var Browser = {
-		IsUCS: function () {
-			var ucs = document.querySelector('div#ucs');
-			if (ucs === null) {
-				return false;
-			}
-			return true;
-		},
-		IsTextSearch: function () {
-			if (location.pathname !== '/' && !/^\/(search|webhp)$/.test(location.pathname)) {
-				return false;
-			}
-			var q = (/&/.test(location.search)) ? location.search.slice(1).split('&') : [];
-			var h = (/#/.test(location.hash)) ? location.hash.slice(1).split('&') : [];
-			var o = {};
-			for (var i = 0; i < h.length; ++i) {
-				q.push(h[i]);
-			}
-			for (var i = 0; i < q.length; ++i) {
-				var a = q[i].split('=');
-				o[a[0]] = a[1];
-			}
-			if (typeof o.tbm === 'undefined' || o.tbm === '') {
-				return true;
-			}
-			return false;
-		},
-		QueryHashToArray() {
-			if (location.pathname !== '/' && !/^\/(search|webhp)$/.test(location.pathname)) {
-				return false;
-			}
-			var o = {};
-			var q = (/&/.test(location.search)) ? location.search.slice(1).split('&') : [];
-			var h = (/#/.test(location.hash)) ? location.hash.slice(1).split('&') : [];
-			for (var i = 0; i < h.length; ++i) {
-				q.push(h[i]);
-			}
-			for (var i = 0; i < q.length; ++i) {
-				var a = q[i].split('=');
-				o[a[0]] = a[1];
-			}
-			return o;
-		},
-		RewriteURI: function (t, q) {
-			Model.START = Model.GetStartRange(t);
-			Model.END = Model.GetEndRange();
-			if (t === 'none') {
-				var path = '/search?hl=ja&site=webhp&biw=810&bih=1306&q=' + q + '&oq=' + q + '&ie=UTF-8&tbm=';
-				window.location.href = path;
-			}			else {
-				var range = 'tbs=cdr:1,cd_min:' + Model.START + ',cd_max:' + Model.END;
-				var path = '/search?hl=ja&site=webhp&biw=810&bih=1306&q=' + q + '&oq=' + q + '&ie=UTF-8&' + range + '&tbm=';
-				window.location.href = path;
-			}
-		},
-		ReplaceToNeutralIfExistRange: function () {
-			var hash = window.location.search;
-			var o = Browser.QueryHashToArray();
-			var q = o.q;
-			var pattern = /(tbs=cdr:1,cd_min:)(.*?)(,cd_max:)/;
-			if (hash.match(pattern) && Browser.IsTextSearch()) {
-				var path = '/search?hl=ja&site=webhp&biw=810&bih=1306&q=' + q + '&oq=' + q + '&ie=UTF-8&tbm=';
-				window.location.href = path;
-			}
-		}
-	};
+        const ucs = document.querySelector('div#ucs');
+        ucs.insertBefore(div, ucs.firstChild);
+      },
+      QuickChange: () => {
+        const range = this.getAttribute('data');
+        const q = Browser.QueryHashToArray().q;
+        Browser.RewriteURI(range, q);
+      }
+    };
 
-	var Main = function () {
-		var Add = function () {
-			var range = Model.GetRangeFromStorage();
-			console.log(range);
-			View.BindElement();
-			View.SetCssState(range);
-			Model.SetRangeToStorage('none');
-		};
+    var Model = {
+      VALUE_PATTERN: /^(m3|m6|y2|y3|y)$/,
+      ARG_PATTERN: /^qdr:(m3|m6|y|y2|y3)$/,
+      TERM: {
+        0: {data: 'none', text: 'Normal'},
+        1: {data: 'm3', text: '3 Month'},
+        2: {data: 'm6', text: '6 Month'},
+        3: {data: 'y', text: '1 Year'},
+        4: {data: 'y2', text: '2 Year'},
+        5: {data: 'y3', text: '3 Year'}
+      }
+    };
 
-		var Purge = function () {
-			View.DeleteElement();
-		};
+    var Browser = {
+      IsUCS: function () {
+        var ucs = document.querySelector('div#ucs');
+        return (ucs === null) ? false : true;
+      },
+      GetRangeByQueryString: () => {
+        const o = Browser.QueryHashToArray();
+        if (typeof o.tbs === 'undefined' || o.tbs === '') {
+          return 'none';
+        }
+        if (Model.ARG_PATTERN.test(o.tbs)) {
+          return RegExp.$1;
+        }
+        return 'none';
+      },
+      IsTextSearch: function () {
+        const o = Browser.QueryHashToArray();
+        if (typeof o.tbm === 'undefined' || o.tbm === '') {
+          return true;
+        }
+        return false;
+      },
+      QueryHashToArray() {
+        let o = {};
+        if (location.pathname !== '/' && !/^\/(search|webhp)$/.test(location.pathname)) {
+          return o;
+        }
+        let q = (/&/.test(location.search)) ? location.search.slice(1).split('&') : [];
+        const h = (/#/.test(location.hash)) ? location.hash.slice(1).split('&') : [];
+        for (let i = 0; i < h.length; ++i) {
+          q.push(h[i]);
+        }
+        for (let i = 0; i < q.length; ++i) {
+          const a = q[i].split('=');
+          o[a[0]] = a[1];
+        }
+        return o;
+      },
+      RewriteURI: function (range, q) {
+        const pre_path = '/search?hl=ja&site=webhp&biw=810&bih=1306&q=' + q + '&oq=' + q + '&ie=UTF-8&';
+        if (Model.VALUE_PATTERN.test(range)) {
+          window.location.href = pre_path + 'tbs=qdr:' + range + '&tbm=';
+        } else {
+          window.location.href = pre_path + 'tbm=';
+        }
+      }
+    };
 
-		if (Model.GetRangeFromStorage() === 'none') {
-			Browser.ReplaceToNeutralIfExistRange();
-		}
+    const Main = () => {
+      const Add = () => {
+        if (Browser.IsTextSearch() === true && Browser.IsUCS() === true) {
+          View.BindElement();
+          const range = Browser.GetRangeByQueryString();
+          View.SetCssState(range);
+        }
+      };
 
-		var Observer = new MutationObserver(function (mutations) {
-			if (Browser.IsTextSearch() === true && Browser.IsUCS() === true) {
-				Add();
-			}			else {
-				Purge();
-			}
-		});
+      const Observer = new MutationObserver( () => {
+          Add();
+      });
 
-		var e = document.getElementById('main');
-		if (e !== null) {
-			if (Browser.IsTextSearch() === true && Browser.IsUCS() === true) {
-				console.log('k');
-				Add();
-			}
-			Observer.observe(e, {childList: true});
-		}
-	};
+      const e = document.getElementById('main');
+      if (e !== null) {
+        Add();
+        Observer.observe(e, {childList: true});
+      }
+    };
 
-	Main();
+    Main();
 })();
 
