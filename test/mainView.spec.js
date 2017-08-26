e0cef9a92af5d7b1dbccdcef1938a6bfba88062b
ktrysmt <kotaro.yoshimatsu@gmail.com>
2017-08-27 00:35:16 +0900

    update mainView.spec.js

--- test/mainView.spec.js
+++ test/mainView.spec.js
@@ -1,45 +1,21 @@
 /* eslint-env mocha */
 const expect = require('chai').expect;
 const View = require('../src/mainView.js');
-const Model = require('../src/model.js');
 
 describe('mainView >', () => {
-  const range = 'm3';
-
-  it('CreateElement', () => {
-    const dom = View.CreateElement();
-    const nodes = dom.childNodes;
-
-    expect(nodes[0].nodeName).to.equal('SPAN');
-    expect(nodes[7].nodeName).to.equal('SPAN');
-
-    for (let i = 0; i < Model.TERM.length; ++i) {
-      const key = i + 1;
-      expect(nodes[key].nodeName).to.equal('A');
-      expect(nodes[key].textContent).to.equal(Model.TERM[i].text);
-      expect(nodes[key].getAttribute('data')).to.equal(Model.TERM[i].data);
-    }
+  describe('CreateElement', () => {
+    it('normal', () => {
+      const dom = View.CreateElement();
+      expect(dom.nodeName).to.equal('DIV');
+    });
   });
 
-  it('BindElement', () => {
-    const dom = View.CreateElement();
-    View.BindElement(dom);
-    const target = document.querySelector('div#ucs > .quick-custom-gsearch');
-    expect(target.nodeName).to.equal('DIV');
-  });
-
-  it('SetCssState', () => {
-    View.SetCssState(range);
-    const target = document.querySelector(`.quick-custom-gsearch a[data=${range}]`);
-    expect(target.className).to.equal('active');
-  });
-
-  it('QuickChange', () => {
-    const elm = document.createElement('a');
-    elm.setAttribute('data', Model.TERM[0].data);
-    elm.appendChild(document.createTextNode(Model.TERM[0].text));
-    const event = View.QuickChange.bind(elm);
-    const result = event();
-    expect(result).to.equal('/search?hl=ja&site=webhp&biw=810&bih=1306&q=duckduckgo&oq=duckduckgo&ie=UTF-8&tbm=');
+  describe('BindElement', () => {
+    it('normal', () => {
+      const dom = View.CreateElement();
+      View.BindElement(dom);
+      const target = document.querySelector('div#ucs > .quick-custom-gsearch');
+      expect(target.nodeName).to.equal('DIV');
+    });
   });
 });
